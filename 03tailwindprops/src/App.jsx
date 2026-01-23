5cf6262c00f152db544c5e4cfb4327d7d39b98b1
Shivam Gupta <sg5743006@gmail.com>
2026-01-23 19:55:23 +0530

    Learning Tailwindcss with React

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1 className='text-3xl bg-green-500 p-3 rounded-md'>
+    Vite with Tailwind
+   </h1>
+    <Card />
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <img src="https://images.pexels.com/photos/29746447/pexels-photo-29746447.jpeg" alt="" />
+        <h1 className='text-2xl bg-green-500 p-3 rounded'>
+            card for Photos
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem consectetur repellendus vero nihil repellat magnam fugit explicabo, voluptates illum? Cum temporibus debitis quo eveniet. Facere, tenetur provident. Autem, perspiciatis ea.</p>
+    </div>
+  )
+}
+
+export default Card
