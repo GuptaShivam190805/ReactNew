9d62baa9955875290863c809819fc8a79c77d3e1
Shivam Gupta <sg5743006@gmail.com>
2026-01-25 16:48:03 +0530

    React_rout_WebSite_Change_something

--- 07ReactRouter/src/components/Github/Github.jsx
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
 
-    const data = useLoaderData()
+    const data = useLoaderData();
 
     // const [data, setData] = React.useState([])
     // useEffect(()=>{
